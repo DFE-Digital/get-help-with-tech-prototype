4a21d2eeb89fee022bcd9bb8fe08b09a9182c4d0
Paul Hayes <fofr@users.noreply.github.com>
2020-06-12 15:48:43 +0100

    Create an account sign in flow

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/account', (req, res) => {
+  const hasAccount = req.session.data['has-account']
+
+  if (hasAccount === 'yes') {
+    res.redirect('/account/check-email')
+  } else {
+    res.redirect('/account/create-account')
+  }
+})
+
 module.exports = router
