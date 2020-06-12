46cd3c0da1c4d4408335b18e0dce680d4ac126ff
Paul Hayes <fofr@users.noreply.github.com>
2020-06-12 16:34:13 +0100

    Add JS to mark areas as not ready yet

---
+++ app/assets/javascripts/modules/clear-hidden.js
@@ -0,0 +1,41 @@
+/* globals $ */
+;(function (global) {
+  'use strict'
+
+  var GOVUK = global.GOVUK || {}
+  GOVUK.Modules = GOVUK.Modules || {}
+
+  // Clear values from any hidden conditional inputs on form submission
+  GOVUK.Modules.ClearHidden = function () {
+    this.start = element => {
+      function getClosest (elem, selector) {
+        for (; elem && elem !== document; elem = elem.parentNode) {
+          if (elem.matches(selector)) return elem
+        }
+        return null
+      };
+
+      const form = getClosest($(element).get(0), 'form')
+
+      form.addEventListener('submit', function (e) {
+        e.preventDefault()
+
+        const inputs = form.querySelectorAll('input, textarea')
+        for (const input of inputs) {
+          const conditional = getClosest(input, '.govuk-radios__conditional') || getClosest(input, '.govuk-checkboxes__conditional')
+          if (conditional && conditional.id) {
+            const controller = this.querySelector(`[aria-controls=${conditional.id}]`)
+
+            if (!controller.checked) {
+              input.value = ''
+            }
+          }
+
+          this.submit()
+        }
+      })
+    }
+  }
+
+  global.GOVUK = GOVUK
+})(window)
---
+++ app/assets/javascripts/modules/edge.js
@@ -0,0 +1,24 @@
+/* global $ */
+
+;(function (global) {
+  'use strict'
+
+  var GOVUK = global.GOVUK || {}
+  GOVUK.Modules = GOVUK.Modules || {}
+
+  GOVUK.Modules.Edge = function () {
+    this.start = function (element) {
+      element.on('click', 'a[href="#"], .js-edge', alertUser)
+
+      function alertUser (e) {
+        e.preventDefault()
+        var target = $(e.target)
+        var message = target.data('message') || 'Sorry, this hasn’t been built yet'
+
+        window.alert(message)
+      }
+    }
+  }
+
+  global.GOVUK = GOVUK
+})(window)
