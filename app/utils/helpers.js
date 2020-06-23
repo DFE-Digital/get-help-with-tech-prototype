5a126fd79afe5aeab8b32d78b97507180729341e
Paul Hayes <fofr@users.noreply.github.com>
2020-06-23 09:50:45 +0100

    Add setData helper for nested values

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,4 +1,5 @@
 const getKeypath = require('keypather/get')
+const setKeypath = require('keypather/set')
 
 const generateRandomString = (length) => {
   length = length || 3
@@ -10,7 +11,13 @@ const getDataValue = (data, sections) => {
   return getKeypath(data, sections.map(s => `["${s}"]`).join(''))
 }
 
+const setDataValue = (data, sections, value) => {
+  sections = Array.isArray(sections) ? sections : [sections]
+  return setKeypath(data, sections.map(s => `["${s}"]`).join(''), value)
+}
+
 module.exports = {
   generateRandomString,
-  getDataValue
+  getDataValue,
+  setDataValue
 }
