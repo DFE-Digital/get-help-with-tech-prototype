04a190d1effbfc99bcbf27ac3e7888b78ffa3ea8
Paul Hayes <fofr@users.noreply.github.com>
2020-08-19 18:46:40 +0100

    Prep for research

--- app/data/school-details.js
+++ app/data/school-details.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'John Hanson Community School',
-  type: 'Secondary school',
-  allocation: 68
+  name: 'St John’s Primary and Nursery School',
+  type: 'Primary school',
+  allocation: 32
 }
