08f7420ab99935b6c95668228b40536b67362dc5
Paul Hayes <fofr@users.noreply.github.com>
2020-08-21 09:08:09 +0100

    Update scripts to work with TRUSTS

---
+++ app/data/responsible-body-schools.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    URN: 141369,
+    name: "Ian Ramsey Church of England Academy",
+    rb: "DAYSPRING TRUST",
+    total: 107,
+    phase: "Secondary",
+    type: "Academies",
+    headteacher: "Brian Janes",
+    headteacher_email: "brian.janes@school.gov.uk",
+    fsm_percentage: 11.4
+  },
+  {
+    URN: 139184,
+    name: "Venerable Bede Church of England Academy",
+    rb: "DAYSPRING TRUST",
+    total: 80,
+    phase: "Secondary",
+    type: "Academies",
+    headteacher: "David Airey",
+    headteacher_email: "david.airey@school.gov.uk",
+    fsm_percentage: 19.6
+  }
+]
