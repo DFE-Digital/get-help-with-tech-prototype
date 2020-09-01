2e25fc1674f1a712c35fd5bb79f559d8b528c24e
Paul Hayes <fofr@users.noreply.github.com>
2020-09-01 12:51:17 +0100

    Tweak welcome journey for non order users and non-first users

--- app/data/school-details.js
+++ app/data/school-details.js
@@ -2,5 +2,7 @@ module.exports = {
   name: 'Rice Lane Primary School',
   type: 'Primary school',
   allocation: 162,
-  can_order: false
+  can_order: false,
+  order_user: false,
+  first_user: false
 }
