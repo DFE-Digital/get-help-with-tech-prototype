3a6044c47fbfa607c79fa7d1663f4bb09d48482d
Paul Hayes <fofr@users.noreply.github.com>
2020-12-02 08:47:13 +0000

    Fix linting of session defaults

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -7,40 +7,40 @@ const schoolDetails = require('./school-details')
 module.exports = {
   rb: 'LEO ACADEMY TRUST',
   trust: true,
-  'devicesOrdered': 9,
-  'routersAllocated': 0,
-  'routersOrdered': 0,
+  devicesOrdered: 9,
+  routersAllocated: 0,
+  routersOrdered: 0,
   'who-orders-laptops': 'central',
   'responsible-body': {
-  143628: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    143628: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     },
-  145481: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    145481: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     },
-  142398: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    142398: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     },
-  143375: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    143375: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     },
-  140260: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    140260: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     },
-  145484: {
-    chromebooks: "yes",
-    domain: "leoacademytrust.co.uk",
-    recovery: 'email@email.com'
+    145484: {
+      chromebooks: 'yes',
+      domain: 'leoacademytrust.co.uk',
+      recovery: 'email@email.com'
     }
   },
   features: {
