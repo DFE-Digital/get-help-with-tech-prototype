6596c791944a2665333ab2c03cc4193df17f7614
Paul Hayes <fofr@users.noreply.github.com>
2021-01-18 14:43:45 +0000

    Add fake school requests using Faker

---
+++ app/data/fake-school-requests.js
@@ -0,0 +1,49 @@
+const faker = require('faker')
+const dateFormat = require('dateformat')
+
+faker.locale = 'en_GB'
+
+function generateFakeRequests (count) {
+  const requests = {}
+
+  for (var i = 0; i < count; i++) {
+    requests[i] = {
+      'mobile-contract-type': faker.random.arrayElement(['Pay monthly', 'Pay as you go (PAYG)']),
+      privacy: ['Yes, the privacy statement has been shared'],
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      'mobile-number': faker.phone.phoneNumber('07#########'),
+      date: dateFormat(faker.date.recent(), 'd mmmm h:MMtt'),
+      tag: faker.random.arrayElement([
+        { text: 'Requested', classes: 'govuk-tag--blue' },
+        { text: 'In progress', classes: 'govuk-tag--yellow' },
+        {
+          text: faker.random.arrayElement([
+            'Invalid number',
+            'Unknown number',
+            'Unknown name',
+            'Not eligible',
+            'Not on network',
+            'Not valid'
+          ]),
+          classes: 'govuk-tag--red'
+        },
+        { text: 'Completed', classes: 'govuk-tag--green' },
+        { text: 'Unavailable', classes: 'govuk-tag--grey' }
+      ]),
+      'mobile-network': faker.random.arrayElement([
+        'Three', 'Three',
+        'O2', 'O2', 'O2',
+        'GiffGaff',
+        'Vodafone', 'Vodafone',
+        'EE',
+        'Sky mobile',
+        'Virgin mobile',
+        'BT mobile'
+      ])
+    }
+  }
+
+  return requests
+}
+
+module.exports = generateFakeRequests(40)
