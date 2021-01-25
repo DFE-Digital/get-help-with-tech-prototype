1f188dc694d74034de45843efa89d0e7be38fdae
Paul Hayes <fofr@users.noreply.github.com>
2021-01-25 12:26:38 +0000

    Add unavailable request example

--- app/data/fake-school-requests.js
+++ app/data/fake-school-requests.js
@@ -3,33 +3,43 @@ const dateFormat = require('dateformat')
 
 faker.locale = 'en_GB'
 
+function getRandomStatus () {
+  return faker.random.arrayElement([
+    { text: 'Requested', classes: 'govuk-tag--blue' },
+    { text: 'In progress', classes: 'govuk-tag--yellow' },
+    {
+      text: faker.random.arrayElement([
+        // 'Invalid number',
+        'Unknown number',
+        'Unknown name',
+        'Not eligible',
+        // 'Not on network',
+        'Other problem'
+      ]),
+      classes: 'govuk-tag--red'
+    },
+    { text: 'Completed', classes: 'govuk-tag--green' }
+  ])
+}
+
+function unavailableStatus () {
+  return { text: 'Unavailable', classes: 'govuk-tag--grey' }
+}
+
 function generateFakeRequests (count) {
   const requests = {}
 
   for (var i = 0; i < count; i++) {
+    const lastRequest = i === count - 1
+
     requests[i] = {
       'mobile-contract-type': faker.random.arrayElement(['Pay monthly', 'Pay as you go (PAYG)']),
       privacy: ['Yes, the privacy statement has been shared'],
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       'mobile-number': faker.phone.phoneNumber('07#########'),
       date: dateFormat(faker.date.recent(), 'd mmmm h:MMtt'),
-      tag: faker.random.arrayElement([
-        { text: 'Requested', classes: 'govuk-tag--blue' },
-        { text: 'In progress', classes: 'govuk-tag--yellow' },
-        {
-          text: faker.random.arrayElement([
-            // 'Invalid number',
-            'Unknown number',
-            'Unknown name',
-            'Not eligible',
-            // 'Not on network',
-            'Other problem'
-          ]),
-          classes: 'govuk-tag--red'
-        },
-        { text: 'Completed', classes: 'govuk-tag--green' }
-      ]),
-      'mobile-network': faker.random.arrayElement([
+      tag: lastRequest ? unavailableStatus() : getRandomStatus(),
+      'mobile-network': lastRequest ? 'Comcast' : faker.random.arrayElement([
         'Three', 'Three',
         'O2', 'O2', 'O2',
         'GiffGaff',
